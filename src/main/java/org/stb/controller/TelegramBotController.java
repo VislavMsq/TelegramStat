40da63182ccce701a0906dd4a9ed8fe6954eaa1c
NightBird78 <panbogdan05@gmail.com>
2024-06-21 00:15:48 +0200

    fix e

--- src/main/java/org/stb/controller/TelegramBotController.java
+++ src/main/java/org/stb/controller/TelegramBotController.java
@@ -1,6 +1,6 @@
 package org.stb.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +8,7 @@ import org.stb.bot.MyWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 public class TelegramBotController {
     MyWebhookBot bot;
