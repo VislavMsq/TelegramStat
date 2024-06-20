6b1bdd0a75da81200188d43897635ed883fd2956
NightBird78 <panbogdan05@gmail.com>
2024-06-20 23:49:55 +0200

    fix c

--- src/main/java/org/stb/bot/MyWebhookBot.java
+++ src/main/java/org/stb/bot/MyWebhookBot.java
@@ -1,6 +1,6 @@
 package org.stb.bot;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
@@ -11,7 +11,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.concurrent.ExecutionException;
 
 @Component
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class MyWebhookBot extends TelegramWebhookBot {
 
     private final BotMethods botMethods;
--- src/main/java/org/stb/controller/TelegramBotController.java
+++ src/main/java/org/stb/controller/TelegramBotController.java
@@ -1,19 +1,17 @@
 package org.stb.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.stb.bot.BotMethods;
 import org.stb.bot.MyWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 public class TelegramBotController {
-    BotMethods botMethods;
-    MyWebhookBot bot;
+    private final MyWebhookBot bot;
 
     @PostMapping("/telegram-wh/eva")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
